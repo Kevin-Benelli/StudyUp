6ae71efb41e82a874f3de56e05ab2ef4b887748d
Kevin Benelli <Kevin-Benelli@github.com>
2019-03-11 21:56:57 +0000

    updated hotswap bash script and dockerfile (you can now use docker exec w/ hotswap).

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-  
+
 PARAM=" $1"
 
-cID=$(docker container ls -f "name=docker_database_1" -q)
-IPaddress=$(docker exec $cID grep -oP 'server\K.*?(?=:)' /etc/nginx/nginx.conf)
+IPaddress=$(grep -oP 'server\K.*?(?=:)' /etc/nginx/nginx.conf)
 
 if [ $IPaddress == $PARAM ]; then
      echo $PARAM is currently being used.
 else
-    docker exec $cID  sed -i  "s/${IPaddress}":"/${PARAM}":"/" /etc/nginx/nginx.conf
-    docker exec $cID /usr/sbin/nginx -s reload
-fi
+     sed -i  "s/${IPaddress}":"/${PARAM}":"/" /etc/nginx/nginx.conf
+     /usr/sbin/nginx -s reload
+fi 
